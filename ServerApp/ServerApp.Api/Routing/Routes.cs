fe17b1d7d28027d56dc62eae03a748ae1ba4ed93
hudric <dimahudric@gmail.com>
2024-04-17 23:11:39 +0300

    first endpoint

--- ServerApp/ServerApp.Api/Routing/Routes.cs
+++ ServerApp/ServerApp.Api/Routing/Routes.cs
@@ -1,8 +1,16 @@
 ﻿namespace Hikari.Routing;
 
 internal static class Routes {
-    public static void SetRoutes() {
-        throw new NotImplementedException();
+    private static WebApplication app = null!;
+
+    public static void SetRoutes(WebApplication app) {
+        Routes.app = app;
+
+        Test();
+    }
+
+    private static void Test() {
+        _ = app.MapGet("/test/ping", () => "pong");
     }
 }
 
