6e158e455ed07c026e6b9b8385d515bd4a4d42b3
webHikari <q5503212@gmail.com>
2024-04-17 19:25:19 +0300

    fix main

--- ClientApp/src/components/Main/Main.tsx
+++ ClientApp/src/components/Main/Main.tsx
@@ -4,9 +4,11 @@ import styles from './Main.module.css'
 
 export default function Main() {
     return(
+    <>
         <div className={styles.Main}>
-            <Header />
             <h1>Here is - Winter</h1>
         </div>
+        <Header />
+    </>
     );
 }
